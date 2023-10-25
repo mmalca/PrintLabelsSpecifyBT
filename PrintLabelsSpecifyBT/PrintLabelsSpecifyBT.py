b5c3e9a935c5570ff9d89464ddef7a8af516b111
mmalca <mmalca7@gmail.com>
2023-10-25 15:10:44 +0300

    updated working with watchdog instead of listening on a loop

--- PrintLabelsSpecifyBT/PrintLabelsSpecifyBT.py
+++ PrintLabelsSpecifyBT/PrintLabelsSpecifyBT.py
@@ -1,12 +1,34 @@
-path_to_watch = "your/path"
-print('Your folder path is"',path,'"')
-previous_files_capture = dict ([(f, None) for f in os.listdir (path_to_watch)])
-while 1:
-        current_files_capture = dict ([(f, None) for f in os.listdir (path_to_watch)])
-        added = [f for f in current_files_capture if not f in previous_files_capture]
-        if added:
-            #create a new file with the relevant info in another location (for the printer)
-                ###print("Added: ", ", ".join (added))
-                break
-        else:
-             previous_files_capture = current_files_capture
+import time
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+
+
+if __name__ == "__main__":
+    print("Starting program...")
+    patterns = ["*.csv"]
+    ignore_patterns = None
+    ignore_directories = True
+    case_sensitive = True
+    my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+
+def created_new_file(event):
+    #create a new file with the relevant info on another location
+    print("New file created!")
+
+def create_BT_CSV_file(src_file, dst_file, dst_path):
+    pass
+
+my_event_handler.on_created = created_new_file
+
+path_file_from_specify = "." #"your/path", "." is the current directory
+go_recursively = False #only on the current directory, not sub dirs
+my_observer = Observer()
+my_observer.schedule(my_event_handler, path_file_from_specify, recursive=go_recursively)
+
+my_observer.start()
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    my_observer.sleep()
+    my_observer.join()
